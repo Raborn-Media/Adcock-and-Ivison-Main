7943cf89e215c54966135586141a9831bb3e9e72
Daniel-Miller <danielmillerusmc3043@outlook.com>
2022-09-02 19:12:25 -0500

    stuff

---
+++ frontend/components/Header.js
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import headerStyles from'../styles/Header.module.css'
+import styles from '../styles/Header.module.css'
+const Header = () => {
+  return (
+    <>
+  <div className={headerStyles.header}>
+    <div className={styles.image}>
+    <Image src="/Adcock_logo.svg" alt="Adcock Logo" width={200} height={200} color={#fff}/> </div>
+    <ul>
+            <li>
+                <Link href='/home'>HOME</Link>
+            </li> 
+            <li>
+            <Link href='/your'>YOUR ATTORNEYS</Link>
+            </li>
+            <li>
+                <Link href='/mycase'>YOUR CASE</Link>
+            </li>
+            <li>
+                <Link href='/news'>IN THE NEWS</Link>
+            </li>
+            <li>
+                <Link href='/talk'>TALK TO US</Link>
+            </li>
+        </ul>
+  </div>
+    </>
+    )
+}
+
+export default Header
---
+++ frontend/components/Layout.js
@@ -0,0 +1,18 @@
+import Header from'./Header'
+import styles from'../styles/Layout.module.css'
+import Footer from'./Footer'
+
+const Layout = ({children}) => {
+  return (
+    <>
+    <Header />
+    <div className={styles.container}>
+      <main className={styles.main }>
+        {children}
+      </main>
+    </div>
+    <Footer />
+    </>    
+  )
+}
+export default Layout
