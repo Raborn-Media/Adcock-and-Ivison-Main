1544810230bdcb05f1360cccaaf60a87099c2eaf
Daniel-Miller <danielmillerusmc3043@outlook.com>
2022-09-08 21:02:41 -0500

    first half of adcock homepage

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,38 +1,40 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import headerStyles from '../styles/Header.module.css'
-import styles from '../styles/Header.module.css'
+import Link from "next/link";
+import Image from "next/image";
+import headerStyles from "../styles/Header.module.css";
+import styles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <>
       <div className={headerStyles.header}>
         <div className={styles.image}>
-          <Image src="/Adcock_logo.svg" alt="Adcock Logo" width={400} height={120} />
+          <Image
+            src="/Adcock_logo.svg"
+            alt="Adcock Logo"
+            width={400}
+            height={120}
+          />
         </div>
         <ul>
           <li>
-            <Link href='/home'>HOME</Link>
+            <Link href="/home">HOME</Link>
           </li>
           <li>
-            <Link href='/your'>YOUR ATTORNEYS</Link>
+            <Link href="/your">YOUR ATTORNEYS</Link>
           </li>
           <li>
-            <Link href='/mycase'>YOUR CASE</Link>
+            <Link href="/mycase">YOUR CASE</Link>
           </li>
           <li>
-            <Link href='/news'>IN THE NEWS</Link>
+            <Link href="/news">IN THE NEWS</Link>
           </li>
           <li>
-            <Link href='/talk'>TALK TO US</Link>
+            <Link href="/talk">TALK TO US</Link>
           </li>
         </ul>
       </div>
-      <div className={styles.text}>
-        <p>"Experienced,</p>
-        <p>Agressive and Effective Trial Attorneys</p>
-      </div>
+      
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
