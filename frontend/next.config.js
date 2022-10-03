cf8ff40def9b4865f1dfe0d2b798339485b9bf07
Daniel-Miller <danielmillerusmc3043@outlook.com>
2022-10-03 18:29:41 -0500

    changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'res.cloudinary.com']
+    domains: ["localhost", "res.cloudinary.com"],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
   },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
